905d08a259934f8b4569b1dbe13dd0ad15923dff
john_doe <rusacuma306@gmail.com>
2025-06-02 11:32:26 +0700

    feat: add graphql and redis setup, change app bootstrap

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,7 +1,26 @@
+import { ApolloDriver } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { IS_DEV_ENV } from '../shared/utils/is-dev.util';
+
+import { getGraphQlConfig } from './config/graphql.config';
 import { PrismaModule } from './prisma/prisma.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [ConfigModule.forRoot({
+      ignoreEnvFile: !IS_DEV_ENV,
+      isGlobal: true,
+  }),
+  GraphQLModule.forRootAsync({
+      driver: ApolloDriver,
+      imports: [ConfigModule],
+      useFactory: getGraphQlConfig,
+      inject: [ConfigService],
+  }),
+  PrismaModule,
+  RedisModule],
 })
 export class CoreModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
 import { CoreModule } from './core/core.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(CoreModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = app.get(ConfigService);
+
+  app.use(cookieParser(config.getOrThrow<number>('COOKIES_SECRET')));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    })
+  );
+  
+  app.enableCors({
+    origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+    credentials: true,
+    exposedHeaders: ['set-cookie'],
+  });
+
+  await app.listen(config.getOrThrow<string>('APPLICATION_PORT'));
 }
 bootstrap();
